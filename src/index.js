cfc3f908ec4b63784995e54d50ed8e04e6fe551b
Kevin Garcia <69099162+k3gar@users.noreply.github.com>
2024-02-15 16:57:34 -0600

    Revert "Jromerojarj/ref"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import "react-toastify/dist/ReactToastify.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import App from "@/App";
-import React from "react";
-import ReactDOM from "react-dom";
-import { ToastContainer } from "react-toastify";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ToastContainer />
     <img src="/assets/earth-3.png" alt="earth" className="earth-image"></img>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
--- src/utils/validateLocationFormat.js
+++
@@ -1,4 +0,0 @@
-export function validateLocationFormat(str) {
-  const regex = /^[0-9,-.]+$/;
-  return regex.test(str);
-}
