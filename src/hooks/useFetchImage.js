c768462fa0e34403aab40b713db08bad14e1be75
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-02-17 08:13:18 -0600

    Add service to get the images

---
+++ src/hooks/useFetchImage.js
@@ -0,0 +1,46 @@
+import { useEffect, useState, useCallback } from "react";
+
+import { getImageEndpoint, fallbackImageUrl } from "../constants/endpoints";
+import useCoordinatesContext from "./useCoordinatesContext";
+
+const useFetchImage = (date) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [imageData, setImageData] = useState(undefined);
+
+  const { coordinates } = useCoordinatesContext();
+
+  const getData = useCallback(async () => {
+    if (coordinates.isListVisible) {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          getImageEndpoint(coordinates.lat, coordinates.lon, date)
+        );
+
+        if(!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setImageData(data);
+      } catch (error) {
+        setImageData({ date: "Image not found!", url: fallbackImageUrl });
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  }, [coordinates, date]);
+
+  const clearImageData = () => {
+    setImageData(undefined);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  return { imageData, isLoading, clearImageData };
+};
+
+export default useFetchImage;
