1e9a08cbb2a1f1faaae785ac1bea67fe1b6dca25
Kevin Garcia <69099162+k3gar@users.noreply.github.com>
2024-02-19 10:11:21 -0600

    Revert "Solution - fabioguardado"

--- src/hooks/index.js
+++
@@ -1,2 +0,0 @@
-export * from '../hooks/useFetchImage';
-export * from '../hooks/useCoordinatesContext';
--- src/utils/index.js
+++
@@ -1,2 +0,0 @@
-export * from './areValidCoordinates';
-export * from './checkEmptyInputValue';
