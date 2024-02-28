e6a915f837f510df5bc6a1c67731721de53fec01
RodSoriano <rsoriano@ioogo.com>
2024-02-28 09:07:02 -0600

    Update function to arrow function in `App` file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import './styles/main.css'
+import "./styles/main.css"
 
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Header/>
       <Main/>
     </div>
   );
-}
+};
 
 export default App;
