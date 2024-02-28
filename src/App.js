3cb75c520c40cc107186e425887aa3f6678be19b
RodSoriano <rsoriano@ioogo.com>
2024-02-28 11:47:38 -0600

    Add semi-colon per code styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import "./styles/main.css"
+import "./styles/main.css";
 
 const App = () => {
   return (
