6b4e1aadc29cd96293daed2ed410497e6b05921d
RodSoriano <rsoriano@ioogo.com>
2024-02-28 16:33:47 -0600

    Create constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const constants = {
+  NASA_API_KEY: process.env.REACT_APP_NASA_API_KEY,
+  ERROR_IMAGE: process.env.REACT_APP_ERROR_IMAGE,
+  API_URI: process.env.REACT_APP_NASA_API_URI,
+};
+
+export const dates = [
+  "2014-02-01",
+  "2015-02-01",
+  "2016-02-01",
+  "2017-02-01",
+  "2021-02-01",
+  "2018-02-01",
+  "2019-02-01",
+  "2020-02-01",
+  "2013-02-01"
+];
