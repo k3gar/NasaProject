2df5cd75518d12572c7566b6af52cbd446bf7622
RodSoriano <rsoriano@ioogo.com>
2024-02-28 22:43:21 -0600

    Add `coordinatesOnly()` method

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,3 +1,7 @@
+export const coordinatesOnly = (input) => {
+  return input.replace(/[^0-9\.\-,]/g, "");
+};
+
 export const validateCoordinates = (input, setErrorMessage) => {
   const coordinateRegex = /^\s*(-?\d+(\.\d+)?)\s*,\s*(-?\d+(\.\d+)?)\s*$/;
 
