5bdd35be6aadbb7aa0fe357c20626331d8ad2a8a
RodSoriano <rsoriano@ioogo.com>
2024-02-28 22:39:35 -0600

    Update answer component to be a card slider

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,31 +1,35 @@
-import React from "react";
-import { constants } from "../utils/constants";
+import React, { useState } from "react";
 
-const Answer = ({ searchResults, errorMessage }) => {
-  const ERROR_IMAGE = constants.ERROR_IMAGE;
+const Answer = ({ searchResults }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  return(
-    <div className="search-result">
-      <h2>Your Place</h2>
-      <div className="search-result-grid">
+  const handlePrevious = () => {
+    setCurrentIndex(prevIndex => (prevIndex === 0 ? searchResults.length - 1 : prevIndex - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex(prevIndex => (prevIndex === searchResults.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div>
+      <h2 className="mb-4 text-xl font-semibold">Your Place In</h2>
+      <div className="max-w-md rounded overflow-hidden shadow-lg">
         {searchResults.map((result, index) => (
-          <div className="search-result-data" key={index}>
-            { result ? (
-              <React.Fragment>
-                <p>{result.date}</p>
-                {result.msg === "No imagery for specified date." ? (
-                  <img src={ERROR_IMAGE} alt="Map Image" />
-                ) : (
-                  <img src={result.url} alt="Map Image" />
-                )}
-              </React.Fragment>
-            ) : (
-              <p>No data available</p>
-            )}
+          <div
+            key={index}
+            className={index === currentIndex ? "block" : "hidden"}
+          >
+            <p className="text-center font-bold italic">{new Date(result.date).getFullYear()}</p>
+            <img src={result.url} alt="Satellite Map" className="w-full rounded-full" />
+            <div className="flex justify-between mt-4">
+              <button onClick={handlePrevious} className="px-4 py-2 bg-blue-500 text-white rounded-lg focus:outline-none flex justify-center items-center">Previous</button>
+              <button onClick={handleNext} className="px-4 py-2 bg-blue-500 text-white rounded-lg focus:outline-none flex justify-center items-center">Next</button>
+            </div>
           </div>
         ))}
       </div>
-  </div>
+    </div>
   );
 };
 
