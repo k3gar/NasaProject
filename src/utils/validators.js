6b7b9a3c201477b449dfb192023c9f3186bd69b4
RodSoriano <rsoriano@ioogo.com>
2024-02-28 22:41:01 -0600

    Update validation with `elseif` statement

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,13 +1,11 @@
-export const validateCoordinate = (input, setErrorMessage) => {
+export const validateCoordinates = (input, setErrorMessage) => {
   const coordinateRegex = /^\s*(-?\d+(\.\d+)?)\s*,\s*(-?\d+(\.\d+)?)\s*$/;
 
   if (input.trim() === "") {
     setErrorMessage("");
+  } else if (!coordinateRegex.test(input)) {
+    setErrorMessage("Invalid coordinates format");
   } else {
-    if (!coordinateRegex.test(input)) {
-      setErrorMessage("Invalid coordinates format");
-    } else {
-      setErrorMessage("");
-    }
+    setErrorMessage("");
   }
 };
