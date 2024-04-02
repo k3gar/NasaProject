5af72fba58b8c8788bc429ca9a6d464c396e6d5b
Douglas Navarrete <dranzersv@gmail.com>
2024-04-02 17:42:40 -0600

    agregar HOC para que funcione react-query

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import './styles/main.css'
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Header from './components/Header';
+import Main from './components/Main';
 
+import './styles/main.css';
+
+const queryClient = new QueryClient();
 
 function App() {
+  //añadí un proveedor envolviendo la app para que funcione react/query en el componente NasaCard
   return (
-    <div className="App">
-      <Header/>
-      <Main/>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Header />
+        <Main />
+      </div>
+    </QueryClientProvider>
   );
 }
 
